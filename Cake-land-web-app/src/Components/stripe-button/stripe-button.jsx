28a03ba027a11da66481038657ef7ee1ce76fc8b
sanket <sanketsabale9767@gmail.com>
2021-05-12 22:55:35 +0530

    cart clear after orders product

--- Cake-land-web-app/src/Components/stripe-button/stripe-button.jsx
+++ Cake-land-web-app/src/Components/stripe-button/stripe-button.jsx
@@ -4,24 +4,27 @@ import StripeCheckout from 'react-stripe-checkout';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../Redux/Cart/cart.selector';
 import { connect } from 'react-redux';
+import firebase from 'firebase';
 import { AllTransactionData, UserOrder } from '../../Firebase/firebase.util';
+import { ResetItems } from '../../Redux/Cart/cart.actions';
 
-const StripeCheckoutButton = ({price,cartItems}) => {
+const StripeCheckoutButton = ({price,cartItems,ResetItems}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51Ip4wpSGbelzIZUmeyVdhz3SY8YEcTCzszZttZ4HLyiAO6vvCMVr9KdnZeJ9giuVyGKzOUenCMjK4U3vKwpWUCC300xnhQlWir';
     const onToken = token => {
         token.price = price
         token.cartItems = cartItems
         console.log(token);
-        //For Store data in firebase
-        AllTransactionData(token);
-        UserOrder(token);
-        
-
-        
-        alert('Order successfull');
-        
-
+        var user = firebase.auth().currentUser;
+        if(user){
+            AllTransactionData(token);
+            UserOrder(token);
+            ResetItems({data:"one"})
+            alert("Order Successfull")
+            
+        }else{
+            alert("You Need to Sign In")
+        }   
     }
     return ( 
         <StripeCheckout 
@@ -44,5 +47,9 @@ const mapStateToProps = createStructuredSelector({
     cartItems : selectCartItems,
     total:selectCartTotal
 });
+
+const mapDispatchToProps = dispatch => ({
+    ResetItems : item => dispatch(ResetItems(item))
+  })
  
-export default connect(mapStateToProps)(StripeCheckoutButton);
+export default connect(mapStateToProps,mapDispatchToProps)(StripeCheckoutButton);
